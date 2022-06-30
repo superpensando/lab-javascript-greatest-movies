d6241df1bc0715a457743d6e1815dba49d130828
laroca <laroca73@gmail.com>
2022-06-30 20:58:47 +0200

    Add include movies

--- src/movies.js
+++ src/movies.js
@@ -1,3 +1,6 @@
+const movies = require('../src/data');
+console.log(movies);
+
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
